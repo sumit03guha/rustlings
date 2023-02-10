4912dff2f97fa015fba30d69dce782e0618743c1
Sumit Guha <sumit03guha@gmail.com>
2023-02-10 23:45:17 +0530

    finished strings2

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -2,7 +2,7 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
