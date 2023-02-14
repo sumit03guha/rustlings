2d4c702787fdfe0d63e20960fe0f5bf8d9b91075
Sumit Guha <sumit03guha@gmail.com>
2023-02-15 00:38:19 +0530

    finished hashmaps2

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,7 +11,7 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -37,6 +37,8 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        basket.insert(Fruit::Banana, 1);
+        basket.insert(Fruit::Pineapple, 1);
     }
 }
 
